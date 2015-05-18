355e2e7dbfdd3b86bc1fd92a237221ec8978a8f9
Dayne Broderson <dayne@alaska.edu>
2015-05-17 17:10:33 -0800

    working detection of n1nj4net dns

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'broderson@gmail.com'
 license 'all_rights'
 description 'Installs/Configures d-base'
 long_description 'Installs/Configures d-base'
-version '0.1.1'
+version '0.1.3'
 
 depends 'chef-client'
 depends 'resolver', '~> 1.3.0'
--- recipes/_resolver.rb
+++ recipes/_resolver.rb
@@ -1,5 +1,5 @@
-dns_server_options = search(:node, 'tag:dns-server', filter_result: {'ip' => ['ipaddress']})
-dns_servers = dns_server_options.collect(&:values)
+dns_server_options = search(:node, 'tags:dns-server', filter_result: {'ip' => ['ipaddress']})
+dns_servers = dns_server_options.collect(&:values).flatten
 dns_servers.push '8.8.8.8' # always give me a google DNS
 
 node.override['resolver']['nameservers'] = dns_servers
