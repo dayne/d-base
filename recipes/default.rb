65efd54801c48c65686db77e05a3edd539db1744
Dayne Broderson <dayne@alaska.edu>
2015-07-03 14:28:20 -0800

    rev version

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,10 +3,8 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
-
 include_recipe 'd-base::_common'
 
 if node['platform'] == "raspbian"
  include_recipe 'd-base::_pi'
 end
-
