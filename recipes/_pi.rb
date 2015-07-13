43acfdfa9f77a1c2467fdbae6616c307efc63efb
Dayne Broderson <dayne@alaska.edu>
2015-07-12 16:11:59 -0800

    Pi upgrades: adding adafruit repo and a packages

--- recipes/_pi.rb
+++ recipes/_pi.rb
@@ -1,3 +1,12 @@
+# adafruit apt repo
+apt_repository 'adafruit' do
+  uri "http://apt.adafruit.com/raspbian/"
+  components ['main']
+  key 'https://apt.adafruit.com/apt.adafruit.com.gpg.key'
+  distribution 'wheezy'
+  action [:remove, :add]
+end
+
 # us keyboard
 replace_or_add "us keyboard" do
   path "/etc/default/keyboard"
@@ -8,7 +17,13 @@ end
 include_recipe 'd-base::_say'
 
 node.default['packages'].merge!( {
-  'dnsutils'  => 'install', 
-  'mpg123'    => 'install',
-  'mplayer'   => 'install'
+  'mplayer'   => 'install',
+  'git-core'  => 'install',
+  'build-essential' => 'install',
+  'libi2c-dev' => 'install',  # for WiringPi libraryies
+  'node' => 'install',
+  'nodejs' => 'install',
+  'wiringpi' => 'isntall'
 })
+
+
