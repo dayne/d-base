d0dd14a33eac42f407d1f4b97fc4cab11d4a5738
Dayne Broderson <dayne@alaska.edu>
2017-02-20 23:22:48 -0900

    chromium for x61

--- recipes/x61.rb
+++ recipes/x61.rb
@@ -4,6 +4,10 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
+packages = %w( chromium-browser )
+
+package packages
+
 include_recipe "d-base::default"
 
 include_recipe 'minecraft-installer-ubuntu'
