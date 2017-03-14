d9fc8291e7d1bcc8f23817856fd6279ec8618150
Dayne Broderson <broderson@gmail.com>
2017-03-14 08:07:04 -0800

    gitkraken v2.2 upgrade

--- recipes/_gitkraken.rb
+++ recipes/_gitkraken.rb
@@ -1,19 +1,22 @@
 node.default['gitkraken']['url'] = \
              'https://release.gitkraken.com/linux/gitkraken-amd64.deb'
-node.default['gitkraken']['version'] = '2.1.0'
-# sha256sum
+node.default['gitkraken']['version'] = '2.2.0'
+
+# curl https://release.gitkraken.com/linux/gitkraken-amd64.deb  | sha256sum
 node.default['gitkraken']['checksum'] = \
-    'bc0f454bf840634a88a47d1c5d5c0491d6c3e9d3bbde0dc8ed592690c62b145c' # v2.1.0
+    '321f4a58a7426a690e3af8c5d0729deb2882db721c6e0eb7948a12021500eb00' # v2.2.0
+  # 'bc0f454bf840634a88a47d1c5d5c0491d6c3e9d3bbde0dc8ed592690c62b145c' # v2.1.0
   # '14afeded24a4e0b2737c0d60917c3965f6cccd4b58c1934e9c45b4a65a7ca5ac' # v2.0.1
 
-remote_file ("/tmp/gitkraken-amd64-2.1.0.deb") do
+version = node.default['gitkraken']['version']
+remote_file ("/tmp/gitkraken-amd64-#{version}.deb") do
   source node.default['gitkraken']['url']
   owner 'root'
   checksum node.default['gitkraken']['checksum']
 end
 
 dpkg_package 'gitkraken' do
-  only_if { File.exist?('/tmp/gitkraken-amd64-2.1.0.deb')}
-  source '/tmp/gitkraken-amd64-2.1.0.deb'
+  only_if { File.exist?("/tmp/gitkraken-amd64-#{version}.deb")}
+  source "/tmp/gitkraken-amd64-#{version}.deb"
   action [ :install, :upgrade ]
 end
