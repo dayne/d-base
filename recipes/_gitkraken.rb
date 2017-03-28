fd7fb75972942d28a16b79fecc6a67c32e3f6a10
Dayne Broderson <broderson@gmail.com>
2017-03-27 23:13:57 -0800

    finishing update to latest gitkraken 2.2.1

--- recipes/_gitkraken.rb
+++ recipes/_gitkraken.rb
@@ -1,10 +1,10 @@
 node.default['gitkraken']['url'] = \
              'https://release.gitkraken.com/linux/gitkraken-amd64.deb'
-node.default['gitkraken']['version'] = '2.2.0'
+node.default['gitkraken']['version'] = '2.2.1'
 
 # curl https://release.gitkraken.com/linux/gitkraken-amd64.deb  | sha256sum
 node.default['gitkraken']['checksum'] = \
-   '167fa11239283174bff09e8e03b5cb018811e9af1c22a7129517939f6de3a551'  
+   '167fa11239283174bff09e8e03b5cb018811e9af1c22a7129517939f6de3a551' # v2.2.1
   # '321f4a58a7426a690e3af8c5d0729deb2882db721c6e0eb7948a12021500eb00' # v2.2.0
   # 'bc0f454bf840634a88a47d1c5d5c0491d6c3e9d3bbde0dc8ed592690c62b145c' # v2.1.0
   # '14afeded24a4e0b2737c0d60917c3965f6cccd4b58c1934e9c45b4a65a7ca5ac' # v2.0.1
