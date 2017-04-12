cfd6b3c746a73fccf42ad565976de3048f9e1661
Dayne Broderson <broderson@gmail.com>
2017-04-12 07:54:55 -0800

    moving gitkraken attributes to attributes

--- recipes/_gitkraken.rb
+++ recipes/_gitkraken.rb
@@ -1,15 +1,3 @@
-node.default['gitkraken']['url'] = \
-             'https://release.gitkraken.com/linux/gitkraken-amd64.deb'
-node.default['gitkraken']['version'] = '2.3.1'
-
-# curl https://release.gitkraken.com/linux/gitkraken-amd64.deb  | sha256sum
-node.default['gitkraken']['checksum'] = \
-   'ddb9eec34232e830646633c43bc9adc61afa0eee79500af29918b62780093b2a' # v2.3.1
-  # '167fa11239283174bff09e8e03b5cb018811e9af1c22a7129517939f6de3a551' # v2.2.1
-  # '321f4a58a7426a690e3af8c5d0729deb2882db721c6e0eb7948a12021500eb00' # v2.2.0
-  # 'bc0f454bf840634a88a47d1c5d5c0491d6c3e9d3bbde0dc8ed592690c62b145c' # v2.1.0
-  # '14afeded24a4e0b2737c0d60917c3965f6cccd4b58c1934e9c45b4a65a7ca5ac' # v2.0.1
-
 version = node.default['gitkraken']['version']
 remote_file ("/tmp/gitkraken-amd64-#{version}.deb") do
   source node.default['gitkraken']['url']
