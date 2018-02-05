8bc3da0b23f7b7c60a2243ce7b25a5604cccbee0
Dayne Broderson <broderson@gmail.com>
2018-02-04 16:26:27 -0900

    relax deps
    Signed-off-by: Dayne Broderson <broderson@gmail.com>

--- Berksfile
+++ Berksfile
@@ -1,4 +1,4 @@
-#source 'https://supermarket.chef.io'
+source 'https://supermarket.chef.io'
 source :chef_server
 
 metadata
@@ -10,6 +10,9 @@ cookbook 'virtualbox', git: 'https://github.com/dayne/virtualbox-cookbook.git'
 cookbook 'steam', git: 'https://github.com/dayne/steam-chef.git', branch: 'update_apt'
 
 cookbook 'ruby_install', git: 'https://github.com/dayne/chef-ruby_install.git', branch: 'patch-2'
-cookbook 'chruby_install', git: 'https://github.com/dayne/chef_chruby_install.git'
+cookbook 'chruby_install', git: 'git@github.com:dayne/chef_chruby_install', branch: 'chef13_and_tests'
 
-#cookbook 'd_devbox', git: 'https://github.com/dayne/d_devbox.git'
+cookbook 'd_devbox', git: 'https://github.com/dayne/d_devbox.git'
+
+cookbook 'terraform', git: 'https://github.com/rosstimson/chef-terraform'
+cookbook 'atom', git: 'git@github.com:dayne/chef-atom.git', branch: 'relaxhomebrew'
--- metadata.rb
+++ metadata.rb
@@ -6,18 +6,18 @@ description 'Installs/Configures d-base'
 long_description 'Installs/Configures d-base'
 version '0.4.41'
 
-depends 'chef-client', '~> 7.1'
-depends 'resolver', '~> 2.0'
-depends 'apt', '~> 5.0'
-depends 'chef-sugar', '~> 3.4.0'
-depends 'line', '~> 0.6.3'
-depends 'packages', '~> 1.0.0'
-depends 'openssh', '~> 2.1'
+depends 'chef-client'
+depends 'resolver'
+depends 'apt'
+depends 'chef-sugar'
+depends 'line'
+depends 'packages'
+depends 'openssh'
 
 depends 'bash-CVE-2014-7169'
 depends 'bash-CVE-2014-6271'
 
-depends 'system', '~> 0.11.2'
+depends 'system'
 
 depends 'd_devbox'
 
@@ -26,11 +26,11 @@ depends 'steam', '~> 2.2.1'             # special branch
 depends 'qgis', '~> 0.3.0'
 depends 'minecraft-installer-ubuntu', '~> 0.3.0'
 
-depends 'docker', '~> 2.15'
+depends 'docker'
 
 depends 'ruby_install', '~> 1.1.0'      # special branch
 depends 'chruby_install', '~> 0.1.3'    # special branch
 
 depends 'n1nj4-users'
 
-depends 'nodejs', '>= 3.0.0'
+depends 'nodejs'
