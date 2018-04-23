67a1a5ab158eea2aec2a6e024072d3ba70271b88
Dayne Broderson <broderson@gmail.com>
2018-04-23 10:39:52 -0800

    0.4.44 update hugo to 0.40

--- recipes/_hugo.rb
+++ recipes/_hugo.rb
@@ -1,11 +1,19 @@
 
+=begin
+# notes for next update:
+VER=0.40
+wget https://github.com/gohugoio/hugo/releases/download/v${VER}/hugo_${VER}_Linux-64bit.deb
+HUGO=hugo_${VER}_Linux-64bit.deb
+sha256sum $HUGO
+aws s3 cp $HUGO s3://n1nj4net-public/ --acl public-read
+=end
 
-ver = "0.20.7"
+ver = "0.40"
 url = "http://n1nj4net-public.s3-website-us-west-2.amazonaws.com/hugo_#{ver}_Linux-64bit.deb"
+sha256sum = '8e7d92af4273f4f7050bec062d9f82fc054fb71968be5a4c2157ea8d348d9073'
 # url = "https://github.com/spf13/hugo/releases/download/#{ver}/hugo_#{ver}_Linux-64bit.deb"
 # url = 'https://github-cloud.s3.amazonaws.com/releases/11180687/bdfaf734-2fe4-11e7-8c8b-959e88662cc7.deb' 
 tmp_file= "/tmp/hugo_#{ver}_Linux-64bit.deb"
-sha256sum = '29757538da41d69081501c93fc5c47f2b374a8e990305ab14a2a954264a67c7d'
 
 remote_file ( tmp_file ) do
   source url
